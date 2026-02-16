34c7bb1efdeb37079c304c483cdd913739f5ca7a
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:40:52 +0000

    Fix: refactored values to variables.tf and tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,23 @@ variable "my_ip_address" {
   description = "My public IP address for bastion SSH access"
   type        = string
 }
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of existing AWS key pair for SSH access"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
