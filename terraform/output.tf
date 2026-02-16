a31b2a2c632ddb46098806fb382c73eab0d81938
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:47:35 +0000

    Fix: add descriptions to output.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,14 @@
 output "dev_public_ip" {
-  value = aws_instance.dev.public_ip
+  description = "Public IP of the DEV EC2 instance"
+  value       = aws_instance.dev.public_ip
 }
 
 output "prod_public_ip" {
-  value = aws_instance.prod.public_ip
+  description = "Public IP of the PROD EC2 instance"
+  value       = aws_instance.prod.public_ip
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
+  description = "ECR repository URL for Docker image pushes"
+  value       = aws_ecr_repository.app.repository_url
 }
