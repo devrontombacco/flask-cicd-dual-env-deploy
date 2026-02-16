4d923f0cc3acfddae08435a37735855d4d6a9119
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:15:48 +0000

    Add IAM Role

---
+++ terraform/iam.tf
@@ -0,0 +1,17 @@
+# IAM Role so that EC2 can access ECR 
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_ecr_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
