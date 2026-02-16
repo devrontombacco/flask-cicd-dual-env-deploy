b3c5cac3009a81a6cbdb610d3fa01572b2532d3e
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:10:46 +0000

    Fix: correct error for ingress traffic on port 5000

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "sg_ec2" {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip_address}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
