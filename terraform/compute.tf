9067232d6d93fc5b5970b2973c49a39805c05e84
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:03:45 +0000

    add ec2 instances to public subnet

---
+++ terraform/compute.tf
@@ -0,0 +1,43 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "ec2_prod" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  provider                    = aws.ireland
+  key_name                    = "my_aws_key"
+  associate_public_ip_address = true
+  #vpc_security_group_ids      = [aws_security_group. ]
+
+  tags = {
+    Name = "ec2_prod"
+  }
+}
+
+resource "aws_instance" "ec2_dev" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  key_name                    = "my_aws_key"
+  associate_public_ip_address = true
+  #vpc_security_group_ids      = [aws_security_group. ]
+
+  tags = {
+    Name = "ec2_dev"
+  }
+}
