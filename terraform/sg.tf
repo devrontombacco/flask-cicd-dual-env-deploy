5fe500338c123089144b5c7a7069fcc38b186814
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-16 18:09:02 +0000

    Fix: Add SG code for EC2s

---
+++ terraform/sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Allow SSH + TCP on port 5000 for Flask app"
+  vpc_id      = aws_vpc.vpc_main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_ec2"
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "my_ip_address" {
+  description = "My public IP address for bastion SSH access"
+  type        = string
+}
