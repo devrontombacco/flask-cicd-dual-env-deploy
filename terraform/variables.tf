69a9a59c27f0752cd0b48356f55743fd9d20442b
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-17 07:08:26 +0000

    remove ami_id from variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,11 +3,6 @@ variable "my_ip_address" {
   type        = string
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
