21a60a7de50bf7c4b095bc6ca92689061124eab8
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-17 22:58:04 +0000

    add force_delete to ecr_repository resource

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "app" {
   name                 = "flask-cicd-dual-env-deploy"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
