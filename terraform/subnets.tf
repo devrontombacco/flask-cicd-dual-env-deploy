508766da52895a2a0ebdff2c7fbc8fdc891ee242
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-17 07:54:45 +0000

    fix: correct subnets reference

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc_main.id
-  cidr_block              = "10.5.0.0/24"
+  cidr_block              = var.public_subnet_cidr
   availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
   tags = {
